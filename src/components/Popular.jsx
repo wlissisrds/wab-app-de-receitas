4cad3f5de6e94580ddee7164fee6000eeb200d6c
wlissis <wlissisdev@outlook.com>
2022-04-24 22:15:41 -0300

    Criando carrousel com slide

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
+
+//style
 import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 // import { Container } from './styles';
 
@@ -23,24 +27,33 @@ function Popular() {
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <Wrapper>
-            <h3>Popular Picks</h3>
-            {popular.map((recipe) => {
-              return (
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            padding: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
                 </Card>
-              );
-            })}
-          </Wrapper>
-        );
-      })}
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+      );
     </div>
-  );
-}
+  )};
 
 const Wrapper = styled.div`
   margin: 4rem 0rem;
@@ -49,12 +62,39 @@ const Wrapper = styled.div`
 const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
-  overflow:hidden;
+  overflow: hidden;
+  position: relative;
 
   img {
-    border-radius:2rem ;
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
     
   }
+  p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
 `;
 
 export default Popular;
